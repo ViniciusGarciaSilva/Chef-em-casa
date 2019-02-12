4c642d8e4939a4fdf2f4c81395df7279ba9fd30a
ViniciusGarciaSilva <vinicius.garcia.silva@usp.br>
2019-02-12 14:01:15 -0200

    Page, controller and view

---
+++ src/modules/menus/menus.controller.tsx
@@ -0,0 +1,9 @@
+import * as React from 'react';
+
+export default class MenusController extends React.Component<any, any> {
+  render() {
+    return (
+      null
+    );
+  }
+}
---
+++ src/modules/menus/menus.page.tsx
@@ -0,0 +1,11 @@
+import * as React from 'react';
+import MenusController from './menus.controller';
+
+export default class MenusPage extends React.Component<any, any> {
+  render() {
+    return (
+      <MenusController/>
+    );
+  }
+};
+
