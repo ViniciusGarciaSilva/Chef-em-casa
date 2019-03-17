379e994d86a2378d7ea93024e812a3bf0872725c
Pedro Nunes <pedroj.nunes0@gmail.com>
2019-03-17 12:00:20 -0300

    tentativa de chamar o back - WIP

--- src/modules/menus/orders/general/orders.controller.tsx
+++ src/modules/menus/orders/general/orders.controller.tsx
@@ -2,6 +2,8 @@ import * as React from 'react';
 import OrdersView from './orders.view';
 import { Order } from "../../../../model/order.model";
 import { getOrders } from '../../../../data/orders.data';
+import prato1 from '../../../../images/mock/prato1.png';
+import axios from 'axios';
 
 export default class OrdersController extends React.Component<any, any> {
   constructor(props: any) {
@@ -13,6 +15,26 @@ export default class OrdersController extends React.Component<any, any> {
   
   componentDidMount() {
     const orders: Order[] = getOrders("");
+    var order: Order;
+    console.log('log')
+    axios.get('localhost:5000/order')
+    .then(response => {
+      
+      console.log(response);
+      order.id = response.data.id;
+      order.providerId = response.data.providerId;
+      order.customerId = response.data.customerId;
+      order.description = response.data.description;
+      order.schedule = response.data.schedule;
+      order.price = response.data.price;
+      order.status = response.data.status;
+      order.customerName = 'DB'
+      order.img = prato1;
+      order.details.appetizer = '';
+      order.details.main = '';
+      order.details.dessert = '';
+      orders.push(order);
+    })
     this.setState({orders: orders});
   }
   
